a0a40a6a7fa8939b4dbfceabaa8e7d5465dc8e48
Olivier Levitt <ungawa14@gmail.com>
2020-01-13 18:33:09 +0100

    Generate random string

--- app/src/main/java/fr/levitt/profiling/controller/sql/ReverseHashController.java
+++ app/src/main/java/fr/levitt/profiling/controller/sql/ReverseHashController.java
@@ -1,6 +1,7 @@
 package fr.levitt.profiling.controller.sql;
 
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,7 @@ import fr.levitt.profiling.service.Hasher;
 @RestController
 @RequestMapping("/sql")
 public class ReverseHashController {
-	
+
 	@Autowired
 	private ReverseHashRepository reverseHashRepository;
 
@@ -24,10 +25,10 @@ public class ReverseHashController {
 		if (hash == null || hash.length() != 64) {
 			throw new IllegalArgumentException("Invalid hash");
 		}
-		
+
 		ReverseResult result = new ReverseResult();
 		result.setHash(hash);
-		
+
 		for (int i = 0; i < 1000*1000; i++) {
 			String sourceToTest = generateRandomInput();
 			if (Hasher.applySha256(sourceToTest) != null && Hasher.applySha256(sourceToTest).equals(hash)) {
@@ -35,7 +36,7 @@ public class ReverseHashController {
 				return result;
 			}
 		}
-			
+
 		List<ReverseHash> foundHashs = reverseHashRepository.findByHash(hash);
 		if (foundHashs.size() > 0) {
 			result.setSource(foundHashs.get(0).getSource());
@@ -45,13 +46,23 @@ public class ReverseHashController {
 		}
 		return result;
 	}
-	
+
 	private String generateRandomInput() {
-		return "";
+		int leftLimit = 97; // letter 'a'
+		int rightLimit = 122; // letter 'z'
+		int targetStringLength = 10;
+		Random random = new Random();
+
+		String generatedString = random.ints(leftLimit, rightLimit + 1)
+				.limit(targetStringLength)
+				.collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+				.toString();
+
+		return generatedString;
 	}
-	
+
 	public static class ReverseResult {
-		
+
 		private String source, hash;
 
 		public String getSource() {
@@ -69,8 +80,8 @@ public class ReverseHashController {
 		public void setHash(String hash) {
 			this.hash = hash;
 		}
-		
-		
+
+
 	}
-	
+
 }
